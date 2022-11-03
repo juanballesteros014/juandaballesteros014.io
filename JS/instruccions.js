3c4424f0804686f6f51b34aa5b3cc4e2ed9b4967
kollahens <77521901+juanballesteros014@users.noreply.github.com>
2022-11-02 19:22:43 -0500

    woejhf

---
+++ JS/instruccions.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado');
+    let posicionObj1 = animacion.getBoundingClientRect().top;
+    console.log(posicionObj1);
+    let tamañoDePantalla = window.innerHeight;
+
+    if(posicionObj1 < tamañoDePantalla){
+        animacion.style.animation = 'mover 1s ease-out'
+        }
+    })
